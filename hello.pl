b6f35f120bb9684427a1ff3d8beb5af47657fda9
mmuheem <mmm2603@gmail.com>
2022-03-20 18:12:27 +0530

    adding hello.pl script

---
+++ hello.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+=begin comment
+
+Description: Simple script to print statment.
+Author: Malik Muheem.
+
+=end comment
+=cut
+
+use strict;
+use warnings;
+
+sub main() {
+	my $name = "Malik Muheem";
+	print "Hello ".$name." welcome to Perl class!"; # dot is used for string concatenation.
+}
+
+main();
